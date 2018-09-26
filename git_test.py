263e2e40b79577af1528d86015882087094a773a
Hieu1050 <43056834+Hieu1050@users.noreply.github.com>
2018-09-26 11:03:37 +0700

    Delete git_test.py

--- git_test.py
+++
@@ -1,8 +0,0 @@
-<<<<<<< HEAD
-print ("hehe")
-print ("hoho")
-print ("haha")
-print ("Day la lan test dau tien :)))))))))")
-=======
-print ("Vua sua xong!")
->>>>>>> b4369a7a0a27d09afb748bc5e7abb86a3b3f5fde
